006a7c5445ece805e6bf24cb264914449ec984c0
hridoy6560 <mdhridoymia667@gmail.com>
2025-08-26 12:07:20 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,20 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker installing...');
-  self.skipWaiting();
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('wallet-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/service-worker.js'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker activating...');
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
